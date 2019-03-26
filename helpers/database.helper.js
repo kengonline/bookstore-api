3de3f2e1d6910ab4521dc843fce522268b79a2a6
kengonline <keang2537@hotmail.com>
2019-03-26 18:16:26 +0700

    Add return value from callback

--- helpers/database.helper.js
+++ helpers/database.helper.js
@@ -2,15 +2,19 @@ const pool = require('../configs/database.config');
 
 const queryTransaction = async (callback) => {
     const conn = await pool.getConnection();
+    let result = undefined;
+
     try {
         await conn.beginTransaction();
-        await callback(conn);
+        result = await callback(conn);
         await conn.commit();
     } catch (e) {
         await conn.rollback();
         console.log(e.message, e.stack)
     }
     conn.release();
+
+    return result;
 }
 
 const getConnection = async () => await pool.getConnection()
