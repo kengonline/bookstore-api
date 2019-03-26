0cbbfa349904151375cb5880b5b058d87753faff
kengonline <keang2537@hotmail.com>
2019-03-26 16:40:17 +0700

    Add error code

--- errors/BusinessError.js
+++ errors/BusinessError.js
@@ -1,9 +1,10 @@
 const { TYPES: { BUSINESS_ERROR } } = require('../constants/error.constant')
 
 class BusinessError extends Error {
-    constructor(message, args) {
+    constructor({ message, code }, args) {
         super(message);
         this.name = BUSINESS_ERROR;
+        this.code = code;
         this.args = args;
         this.httpStatus = 400;
     }
--- errors/UnauthorizedError.js
+++ errors/UnauthorizedError.js
@@ -1,9 +1,11 @@
 const { TYPES: { UNAUTHORIZED_ERROR } } = require('../constants/error.constant')
 
 class AuthorizedError extends Error {
-    constructor() {
-        super("Unauthorized");
+    constructor(data = {}) {
+        const { code = undefined, message = "Unauthorized" } = data;
+        super(message);
         this.name = UNAUTHORIZED_ERROR;
+        this.code = code;
         this.httpStatus = 401;
     }
 }
