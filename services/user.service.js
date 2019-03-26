a0fc394336636ec6d2690fec4378c4c30991cdbe
kengonline <keang2537@hotmail.com>
2019-03-26 18:17:15 +0700

    Add register function

--- services/user.service.js
+++ services/user.service.js
@@ -1,13 +1,38 @@
+const crypto = require('crypto');
+
+const { STATUS } = require('../constants/user.constant');
+const { findOneByEmail, insertUser } = require('../repositories/user.repository')
+const { queryTransaction } = require('../helpers/database.helper')
 const BusinessError = require('../errors/BusinessError')
+const ErrorCode = require('../assets/error_code.json')
 
-const { findOneByEmail } = require('../repositories/user.repository')
+const genRandomString = (length) => {
+    return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+};
 
-const { getConnection, queryTransaction } = require('../helpers/database.helper')
+const getEncryptPassword = (password) => {
+    const salt = genRandomString(16);
+    const hash = crypto.createHmac('sha512', salt);
+    hash.update(password);
+    const value = hash.digest('hex');
+    return {
+        salt: salt,
+        encrpytPassword: value
+    };
+};
 
-const register = async ({ email, password, firstName, lastName }) => {
-    if (!(await findOneByEmail(email))) {
-        throw new BusinessError("This email is taken.", { email })
+const register = async (data = {}) => {
+    if (await findOneByEmail(data.email)) {
+        throw new BusinessError(ErrorCode.USER_400_001)
     }
+
+    const { salt, encrpytPassword } = getEncryptPassword(data.password)
+
+    const result = await queryTransaction(async conn => {
+        return await insertUser(conn, { ...data, password: encrpytPassword, status: STATUS.ACT_ACTIVE, salt });
+    })
+
+    return result
 }
 
 module.exports = {
