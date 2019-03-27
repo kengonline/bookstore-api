ffe045632e66dbae07bfa773ea3141c43b9bca82
kengonline <keang2537@hotmail.com>
2019-03-27 18:05:26 +0700

    Add endpoint to create publishers

--- routes/publisher.route.js
+++ routes/publisher.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-const { wrapper } = require('../helpers/route.helper');
+const { wrapper, secureWrapper } = require('../helpers/route.helper');
+const UserService = require('../services/user.service');
 const PublisherService = require('../services/publisher.service');
 
 router.get('/', (req, res, next) => wrapper(req, res, next, async () => {
@@ -8,6 +9,16 @@ router.get('/', (req, res, next) => wrapper(req, res, next, async () => {
     const result = await PublisherService.getList(criteria)
 
     res.send(result)
+}))
+
+router.post('/', (req, res, next) => secureWrapper(req, res, next, async () => {
+    const { name, status, note, website } = req.body
+
+    const profile = await UserService.getProfileByToken(req.cookies.token);
+
+    const data = { name, status, note, website };
+    await PublisherService.create(data, profile)
+    res.send()
 }))
 
 module.exports = router;
--- services/publisher.service.js
+++ services/publisher.service.js
@@ -1,9 +1,34 @@
+const moment = require('moment')
+const { DATABASE_FORMAT } = require('../constants/datetime.constant');
+const { STATUS } = require('../constants/publisher.constant');
 const PublisherRepository = require("../repositories/publisher.repository")
+const { queryTransaction } = require('../helpers/database.helper')
+const BusinessError = require('../errors/BusinessError')
+const ErrorCode = require('../assets/error_code.json')
 
 const getList = async (criteria) => {
     return await PublisherRepository.findByCriteria(criteria);
 }
 
+const create = async (data, userContext) => {
+    if ((await PublisherRepository.findByCriteria({ name: data.name })).length) {
+        throw new BusinessError(ErrorCode.COMMON_400_001, [data.name])
+    }
+
+    const result = await queryTransaction(async conn => {
+        const createdDate = moment().format(DATABASE_FORMAT);
+        return PublisherRepository.insert(conn, {
+            ...data,
+            status: STATUS.ACT_ACTIVE,
+            createdBy: userContext.email,
+            createdDate
+        })
+    })
+
+    return result;
+}
+
 module.exports = {
-    getList
+    getList,
+    create
 }
