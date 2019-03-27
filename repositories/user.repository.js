f1b5e9d349eba158fc8d20167ed75b5c2168dd26
kengonline <keang2537@hotmail.com>
2019-03-27 16:11:49 +0700

    Add constant TABLE_NAME

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -1,20 +1,22 @@
 const moment = require('moment');
 const { getConnection } = require('../helpers/database.helper')
 
+const TABLE_NAME = "User"
+
 const findOneById = async (id) => {
     const conn = await getConnection();
-    const [rows] = await conn.execute("SELECT * FROM `User` WHERE id = ?", [id])
+    const [rows] = await conn.execute(`SELECT * FROM ${TABLE_NAME} WHERE id = ?`, [id])
     return rows.length ? rows[0] : undefined;
 }
 
 const findOneByEmail = async (email) => {
     const conn = await getConnection();
-    const [rows] = await conn.execute("SELECT * FROM `User` WHERE email = ?", [email])
+    const [rows] = await conn.execute(`SELECT * FROM ${TABLE_NAME} WHERE email = ?`, [email])
     return rows.length ? rows[0] : undefined;
 }
 
 const insertUser = (conn, { email, password, firstName, lastName, status, salt }) => {
-    const query = "INSERT INTO User(email, password, first_name, last_name, status, salt, created_date, created_by) VALUES ?";
+    const query = `INSERT INTO ${TABLE_NAME}(email, password, first_name, last_name, status, salt, created_date, created_by) VALUES ?`;
     const values = [email, password, firstName, lastName, status, salt, moment().format('YYYY-MM-DD HH:mm:ss'), "SYSTEM"];
     return conn.query(query, [[values]])
 }
