bdf439abe160a6a13c856ea40feb0a78689a8248
kengonline <keang2537@hotmail.com>
2019-03-27 14:38:57 +0700

    Change cookie token key to constant

--- routes/authenticate.route.js
+++ routes/authenticate.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { COOKIE } = require('../constants/http.constant');
 const { wrapper } = require('../helpers/route.helper');
 const AuthenticateService = require('../services/authenticate.service');
 
@@ -7,7 +8,7 @@ router.post('/login', (req, res, next) => wrapper(req, res, next, async () => {
     const { email, password } = req.body;
 
     const { profile, token: { key, duration } } = await AuthenticateService.login(email, password);
-    res.cookie('token', key, { maxAge: duration });
+    res.cookie(COOKIE.TOKEN, key, { maxAge: duration });
 
     res.send({ token: key, profile });
 }))
@@ -16,7 +17,7 @@ router.post('/logout', (req, res, next) => wrapper(req, res, next, async () => {
     const { token } = req.cookies;
 
     await AuthenticateService.logout(token)
-    res.cookie('token', undefined, { maxAge: 0 })
+    res.cookie(COOKIE.TOKEN, undefined, { maxAge: 0 })
     res.send()
 }))
 
