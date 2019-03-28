0653407509b4154604b49894b5de1bf81a2fecf3
kengonline <keang2537@hotmail.com>
2019-03-28 13:35:26 +0700

    Add book constants

---
+++ constants/book.constant.js
@@ -0,0 +1,16 @@
+const TYPE = {
+    NOVEL: "novel",
+    MANGA: "manga"
+}
+
+const STATUS = {
+    AN_ANNOUNCE: "AN",
+    PO_PRE_ORDER: "PO",
+    OS_ON_SALE: "OS",
+    RE_RECLAIM: "RE"
+}
+
+module.exports = {
+    TYPE,
+    STATUS
+}
