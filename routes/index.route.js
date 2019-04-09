028db14580b78c38d810e4fefcaf650afa3998fd
kengonline <keang2537@hotmail.com>
2019-04-09 16:50:30 +0700

    Add books to route

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/authenticate', require(`./authenticate.route`));
 router.use('/users', require(`./user.route`));
 router.use('/publishers', require(`./publisher.route`));
+router.use('/books', require(`./book.route`));
 
 module.exports = router;
